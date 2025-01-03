549b9801742406edb5061959374e498f6363e1d8
Hasan Siddiqui <143353727+muhammadhasansiddiqui@users.noreply.github.com>
2025-01-03 22:18:05 +0500

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -6,7 +6,7 @@ export default function Home() {
     <div className="grid grid-rows-[20px_1fr_20px] bg-black items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
     <h1   
     className="text-3xl font-bold text-center p-6 sm:text-5xl bg-yellow-200 rounded-xl box-shadow: 0 4px 6px rgba(255, 255, 255, 0.1);" 
-    >Hello world in next js))</h1>
+    >Hello world in next js</h1>
     
     </div>
   );
